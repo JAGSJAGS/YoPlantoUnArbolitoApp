a56b51ae2504c9042cb7c491bd4b009ead8dbaf1
ealder777 <rafael_ealder@yahoo.es>
2022-11-18 18:05:14 -0400

    Posible solución al problema de los caracteres en el mensaje de error. Estoy usando una libreria que maneja archivos JSON para extraer la string sin alterar su contenido por la codificación

--- app/src/main/java/app/yo_planto/yoplantounarbolito_app/validations/Validations.java
+++ app/src/main/java/app/yo_planto/yoplantounarbolito_app/validations/Validations.java
@@ -2,14 +2,32 @@ package app.yo_planto.yoplantounarbolito_app.validations;
 
 import android.view.View;
 import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class Validations {
 
-    public String validateDatas(String jsonErros, TextView error){
-        String res = jsonErros.replaceAll("\\]", "").replaceAll("\\}\\}", " ").replaceAll("\\[", "").
-                replaceAll("\\{", "\n").replaceAll("\\}", "\n").
-                replaceAll(",", "\n").replaceAll("\"", "").
+    public String validateDatas(String json_erros, TextView error){
+        String obs = "";
+        try {
+            JSONObject root = new JSONObject(json_erros);
+            obs = obs + root.getString("errors");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String res = obs.
+                replaceAll("\\]", "").
+                replaceAll("\\}\\}", " ").
+                replaceAll("\\[", "").
+                replaceAll("\\{", "\n").
+                replaceAll("\\}", "\n").
+                replaceAll(",", "\n").
+                replaceAll("\"", "").
                 replaceAll("errors:", "");
+
         error.setText(res);
         error.setVisibility(View.VISIBLE);
         return res;
